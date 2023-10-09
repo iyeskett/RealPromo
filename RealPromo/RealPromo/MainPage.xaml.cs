6f5beabd332bee9904de46c9e4f22da517cc1503
Felipe Mello <felipesilva.mello27@gmail.com>
2023-10-09 18:45:46 -0300

    Tela de Promoção - ListView

--- RealPromo/RealPromo/MainPage.xaml.cs
+++ RealPromo/RealPromo/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,25 @@ namespace RealPromo
 {
     public partial class MainPage : ContentPage
     {
+        private readonly ObservableCollection<Promocao> lista = new ObservableCollection<Promocao>();
+
         public MainPage()
         {
             InitializeComponent();
+            GetPromocoes();
+            ListViewPromocao.ItemsSource = lista;
+
+            Device.StartTimer(new TimeSpan(00, 1, 00), () =>
+            {
+                lista.Add(new Promocao() { Empresa = "Carrefour", Chamada = $"Celulares em promoção - {DateTime.Now}", Regras = "10 unidades", EnderecoURL = "https://www.carrefrour.com.br" });
+                return true;
+            });
+        }
+
+        private void GetPromocoes()
+        {
+            lista.Add(new Promocao() { Empresa = "Carrefour", Chamada = "TVs em promoção", Regras = "20 unidades", EnderecoURL = "https://www.carrefrour.com.br" });
+            lista.Add(new Promocao() { Empresa = "Carrefour", Chamada = "Celulares em promoção", Regras = "10 unidades", EnderecoURL = "https://www.carrefrour.com.br" });
         }
     }
 }
